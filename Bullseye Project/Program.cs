059efe228d5a3b364a77cf201bae50e25649f124
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-24 13:08:45 -0400

    Add project files.

---
+++ Bullseye Project/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Bullseye
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Dot();
+        }
+
+
+        public static void Dot()
+        {
+            
+            Random rnd = new Random();
+            int PointLocation = rnd.Next(1080 * 1920);
+            //int xAxis = 1080;
+            //int yAxis = 1920;
+            Console.WriteLine(PointLocation);
+            Console.ReadLine();
+
+
+
+        }
+
+
+    }
+}
+
