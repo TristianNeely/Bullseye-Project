97b8b18baf3de9cc1927f7c4b4eee6c0d352ae14
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-29 08:13:56 -0400

    .

--- Bullseye Project/Program.cs
+++ Bullseye Project/Program.cs
@@ -2,12 +2,14 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 
 namespace Bullseye
 {
     class Program
+
+
     {
         static void Main(string[] args)
         {
@@ -18,19 +20,90 @@ namespace Bullseye
 
         public static void Dot()
         {
-            
             Random rnd = new Random();
-            int PointLocation = rnd.Next(1080 * 1920);
-            //int xAxis = 1080;
-            //int yAxis = 1920;
-            Console.WriteLine(PointLocation);
+            int xAxis = rnd.Next(0, 1920);
+            int yAxis = rnd.Next(0, 1080);
+            
+
+
+
+            Console.WriteLine($"The xAxis Cordinate is: {xAxis} / The yAxis Cordinate is:  {yAxis}");
+            //Console.WriteLine(yAxis);
             Console.ReadLine();
 
-
-
         }
+
+        public struct MousePoint
+        {
+            
+        }
+      
+            
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
     }
 }
-
