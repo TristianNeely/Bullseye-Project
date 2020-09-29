72857b806e00f5cd596b5772757b8b1d2dd1e020
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-09-29 11:08:54 -0400

    Anything brakes, come back here

--- Bullseye Project/Form1.cs
+++ Bullseye Project/Form1.cs
@@ -1,18 +1,20 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Threading;
-using System.Drawing;
-using System.Windows;
 
 namespace Bullseye_Project
 {
-    class Mouse
+    public partial class Form1 : Form
     {
-        
+        public Form1()
+        {
+            InitializeComponent();
+        }
     }
 }
-
--- Bullseye Project/Program.cs
+++ Bullseye Project/Program.cs
@@ -3,73 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
-
+using System.Runtime.InteropServices;
+using System.Drawing;
+using System.Threading;
 
 namespace Bullseye
 {
     class Program
-
-
     {
+
         static void Main(string[] args)
         {
 
-            Dot();
+
+            /* RandomPos()*/
+           
+            MousePos();
         }
 
 
-        public static void Dot()
+        public static void MousePos()
         {
             Random rnd = new Random();
             int xAxis = rnd.Next(0, 1920);
             int yAxis = rnd.Next(0, 1080);
-            
-
-
-
             Console.WriteLine($"The xAxis Cordinate is: {xAxis} / The yAxis Cordinate is:  {yAxis}");
-            //Console.WriteLine(yAxis);
-            Console.ReadLine();
+            int myCount = 0;
+
+            while (true)
+            {
+                Thread.Sleep(100);
+                int userX = Cursor.Position.X;
+                int userY = Cursor.Position.Y;
+                Console.WriteLine($"The Mouse Position of X is: {userX}, The Mouse Position of X is: {userY}");
+
+                Console.WriteLine($"The xAxis Cordinate is: {xAxis} / The yAxis Cordinate is:  {yAxis}");
+                /* Random rnd = new Random();
+                 int xAxis = rnd.Next(0, 1920);
+                 int yAxis = rnd.Next(0, 1080);
+
+                 Console.WriteLine($"The Random xAxis Cordinate is: {xAxis} / The Random yAxis Cordinate is:  {yAxis}");
+                */
+                if (myCount <= 100)
+                {
+                    Thread.Sleep(1000);
+                    ++myCount;
+                    Console.WriteLine($"Number of seconds: {myCount}");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("For the love of god. Stop me.");
+                    MousePos();
+                    Console.Clear();
+                }
+
+            }
+
+
+
+
 
         }
 
-        public struct MousePoint
-        {
-            
-        }
-      
-            
+        //public static void RandomPos()
+        //{
+        //    Random rnd = new Random();
+        //    int xAxis = rnd.Next(0, 1920);
+        //    int yAxis = rnd.Next(0, 1080);
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        //    Console.WriteLine($"The xAxis Cordinate is: {xAxis} / The yAxis Cordinate is:  {yAxis}");
+        //    //Console.WriteLine(yAxis);
+        //    Console.ReadLine();
+        //}
 
 
 
