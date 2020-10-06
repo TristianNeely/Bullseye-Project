b6b8b8be7f10c296d20b86ef1d608b9496a29a83
Tristian Neely <Neely_Tristian@student.mahoningctc.com>
2020-10-06 11:12:38 -0400

    safe

--- Bullseye Project/Program.cs
+++ Bullseye Project/Program.cs
@@ -6,35 +6,46 @@ using System.Windows.Forms;
 using System.Runtime.InteropServices;
 using System.Drawing;
 using System.Threading;
+using System.IO;
+
+
 
 namespace Bullseye
 {
     class Program
     {
-
+        static public string documentPath = @"C:/Users/neely_tristian/Desktop/Bullseye Project(UPDATED)/Bullseye Project/Bullseye Project/myData.txt";
         static void Main(string[] args)
         {
 
 
             /* RandomPos()*/
+            
+            normalDifficulty();
 
-            MousePos();
         }
 
-
-        public static void MousePos()
+        public static void EasyDif()
         {
+
+        }
+        public static void normalDifficulty()
+        {
+
+            // Random Pos
             Random rnd = new Random();
             int xAxis = rnd.Next(0, 1920);
             int yAxis = rnd.Next(0, 1080);
             Console.WriteLine($"The xAxis Cordinate is: {xAxis} / The yAxis Cordinate is:  {yAxis}");
             int myCount = 0;
-          
+
 
 
             while (true)
             {
                 Thread.Sleep(100);
+
+                // Mouse Pos
                 int userX = Cursor.Position.X;
                 int userY = Cursor.Position.Y;
                 Console.WriteLine($"The Mouse Position of X is: {userX}, The Mouse Position of Y is: {userY}");
@@ -55,17 +66,17 @@ namespace Bullseye
                 else
                 {
                     Console.WriteLine("For the love of god. Stop me.");
-                    MousePos();
+                    normalDifficulty();
                     Console.Clear();
                 }
-                 if (userX == xAxis && userY == yAxis)
+                if (userX == xAxis && userY == yAxis)
                 {
                     Console.WriteLine("Got It!!");
                     return;
                 }
 
                 // Frozen
-                int BananaX =  xAxis - 1000;
+                int BananaX = xAxis - 1000;
                 int BananaY = yAxis - 1000;
 
                 // Ice Cold
@@ -75,10 +86,14 @@ namespace Bullseye
                 // Cold
                 int KiwiX = xAxis - 401;
                 int KiwiY = yAxis - 401;
-                
+
                 // Warm
                 int AppleX = xAxis - 200;
-                int AppleY = xAxis - 200;
+                int AppleY = yAxis - 200;
+
+                // Spicyyyyyyy
+                int pepperX = xAxis - 140;
+                int pepperY = yAxis - 140;
 
                 // Hot
                 int StrawberryX = xAxis - 80;
@@ -91,7 +106,7 @@ namespace Bullseye
                 // Hit
                 int CantalopeX = xAxis - 5;
                 int CantalopeY = yAxis - 5;
-                
+
 
 
 
@@ -100,7 +115,7 @@ namespace Bullseye
                 {
                     Console.WriteLine("Your INSANE!");
                     Thread.Sleep(90000000);
-                    
+
                 }
 
 
@@ -110,7 +125,8 @@ namespace Bullseye
                 {
                     Console.WriteLine("Your Frozen!");
                     Console.WriteLine("--------------------");
-                   
+
+
 
 
                 }
@@ -120,8 +136,8 @@ namespace Bullseye
                 {
                     Console.WriteLine("Your Ice Cold!");
                     Console.WriteLine("--------------------");
-                    
-                 
+
+
                 }
 
                 //
@@ -129,13 +145,22 @@ namespace Bullseye
                 {
                     Console.WriteLine("Your Cold!");
                     Console.WriteLine("--------------------");
-                   
+
                 }
 
                 //
                 if (AppleX <= userX && AppleY <= userY)
                 {
                     Console.WriteLine("Your Warm!");
+                    Console.WriteLine("--------------------");
+                }
+
+
+                // 
+
+                if (pepperX <= userX && pepperY <= userY)
+                {
+                    Console.WriteLine("Your  Spicyyyyyyyyy!");
                     Console.WriteLine("--------------------");
                 }
 
@@ -155,76 +180,45 @@ namespace Bullseye
 
                 if (CantalopeX <= userX && CantalopeY <= userY)
                 {
-                    
+                
+
 
                     Console.WriteLine("You Win!");
                     Console.WriteLine(myCount);
                     Console.WriteLine("--------------------");
-                    Thread.Sleep(10000000);
 
+                    Console.WriteLine("Do you want to play again: Yes / No");
+                    string result = Console.ReadLine();
+                    Console.WriteLine(result);
+
+                    if (result == "Yes" || result == "y")
+                    {
+                        normalDifficulty();
+                    }
+                    else if (result == "No" || result == "n")
+                    {
+                        Console.WriteLine("Hope you had fun");
+                        Console.WriteLine("Press Any Button To Exit");
+                        Console.ReadKey();
+                        Console.Clear();
+                        Console.ReadLine();
+                        Thread.Sleep(10000000);
+                    }
+
+
+
+                   
+
+                    
                 }
 
-
-
-
-
-
+                
 
             }
-
-
-
-
-
         }
-
-        //public static void RandomPos()
-        //{
-        //    Random rnd = new Random();
-        //    int xAxis = rnd.Next(0, 1920);
-        //    int yAxis = rnd.Next(0, 1080);
-
-
-
-
-        //    Console.WriteLine($"The xAxis Cordinate is: {xAxis} / The yAxis Cordinate is:  {yAxis}");
-        //    //Console.WriteLine(yAxis);
-        //    Console.ReadLine();
-        //}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        // Easy Difficulty
+       
+      
 
     }
 }
